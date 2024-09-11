b1c606a39d5cba4cd5ffa71729342ff7fa075c35
allanpobre <allannobre100608@gmail.com>
2024-09-11 17:09:21 -0300

    OHMAGA
    Co-authored-by: Artasi67 <Artasi67@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function comprar(){
-    
+
+    const pizza = document.getElementsByClassName("btn btn-warning")
+
+    var sabor = pizza.getAttribute('id')
+
+}
+
+function exibirBebidas() {
+    document.getElementById('pizzas').style.zIndex = '1'; // Pizzas vai para trás
+    document.getElementById('bebidas').style.zIndex = '3'; // Bebidas vem para frente
+    document.getElementById('combos').style.zIndex = '1'; 
+}
+
+function exibirPizzas() {
+    document.getElementById('pizzas').style.zIndex = '3'; // Pizzas volta para frente
+    document.getElementById('bebidas').style.zIndex = '1'; // Bebidas vai para trás
+    document.getElementById('combos').style.zIndex = '1'; 
+}
+
+function exibirCombos() {
+    document.getElementById('pizzas').style.zIndex = '1'; // Pizzas volta para frente
+    document.getElementById('bebidas').style.zIndex = '1'; // Bebidas vai para trás
+    document.getElementById('combos').style.zIndex = '3'; 
 }
